6e756285a64e0dfe02f42a040317dfe76ee881ce
aris_giotis <apjoti@gmail.com>
2022-12-12 01:16:01 +0200

    arrays - for loop with of (values --> array's elements)

--- main.js
+++ main.js
@@ -12,3 +12,7 @@ for (let i in arr1) { // i is the index
     console.log(i) // 0 1 2 3 4 -> indices
     console.log(arr1[i]) // 1 2 3 4 5 --> arr1 elements
 }
+
+for (let element of arr2) {
+    console.log(element) // 1,'two',3,null,5 -> array elements
+}

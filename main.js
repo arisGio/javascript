2394d274da6de431ee883eba65a6233a138a1835
aris_giotis <apjoti@gmail.com>
2022-12-12 01:09:00 +0200

    arrays - they are mutable, array elements can change, array = matrix

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 "use strict";
 
-// arrays: mutable collection of objects
 const arr1 = [1,2,3,4,5]
-const arr2 = [1,'two',3,null,5] // mixed array with various types
+const arr2 = [1,'two',3,null,5]
 
 const arr3 = [
     [1,2,3],
     ['one', 'two', 'three'] 
-]// array of arrays
+]
 
-console.log(arr1[0])  // 1
-console.log(arr1[3])  // 4
-console.log(arr3[1][1])  // two
+arr1[2] = 5 // no error, even though arr1 is const, recall what Kyle said: const is the array pointer, not the actual memory content

2da2816df544cfddad55c2f690e2736d8d7e10ff
aris_giotis <apjoti@gmail.com>
2022-12-17 19:17:01 +0200

    while, do while, logical operators - conditions, == vs ===

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 "use strict";
 
-let b = true; // boolean
+let x = 5, y = 10;
 
-console.log(b)
+console.log(x == 5) // true
+console.log(x == 15) // false
+
+console.log(x == '5') // true, type conversion under the hood
+// '5' --> 5 --> 5 == 5 is true
+console.log(x === '5') // false, no type conversion, strict comparison

9f8faffa601750711d2df4a26fcc2b4973859aa3
aris_giotis <apjoti@gmail.com>
2022-12-17 18:47:49 +0200

    objects - convert to string (ie JSON), to send object as data to API or server

--- main.js
+++ main.js
@@ -16,4 +16,4 @@ const student = {
 
 student.grade = 10;
 
-console.log(student.grade) // 10
+console.log(JSON.stringify(student)) // {"firstname":"Alex","lastname":"Alexiou","age":22,"courses":["Computer science","Math"],"teacher":{"name":"Vasilis Vasileiou","course":"Math"},"grade":10}

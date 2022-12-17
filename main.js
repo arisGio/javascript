ccf444640aae1ef2eff518ea308875c5a4c02973
aris_giotis <apjoti@gmail.com>
2022-12-17 19:25:50 +0200

    while, do while, logical operators - while loop

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
 "use strict";
 
-let x = 5, y = 10;
+let x = 0;
 
-console.log(x === 5 && y === 10) // true
-console.log(x === 5 && y === 11) // false
-// BOTH conditions must be true to result in true
-
-console.log(x === 5 || y === 10) // true
-console.log(x === 6 || y === 11) // false
-// AT LEAST on condition must be true
-
-let b = true;
-console.log(b)// true
-console.log(!b)// false
-console.log(!(x === 5))// false
-console.log(!(x === 15))// true
+while ( x < 5 ) {
+    console.log(x);
+    x++;
+}

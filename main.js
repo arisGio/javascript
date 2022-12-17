bda0cbf3e1574d77ea833724aa5b460429cb15b2
aris_giotis <apjoti@gmail.com>
2022-12-17 19:48:41 +0200

    if & switch - switch (many conditions)

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 "use strict";
 
-let age = 20;
+let answer = 9;
 
-if (age < 6) {
-    console.log("You are infant")
-} else if (age < 18) {
-    console.log("You are juvenile")
-} else {
-    console.log("You are adult")
+switch (answer) {
+    case 1:
+        console.log('You are at the right stop')
+        break
+    case 2:
+        console.log('There is a relevat series in channel')
+        break
+    case 3:
+        console.log('There is no Java series')
+        break
+    case 4:
+        console.log('There is a relevant series in channel')
+        break
+    case 5:
+        console.log('There is a relevant series in channel')
+        break
+    default:
+        console.log('You made a wrong choice')
+        break
 }

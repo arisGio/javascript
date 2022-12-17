d2e7703bbc329f0893a9e8b5d39fb4b7b163dfc4
aris_giotis <apjoti@gmail.com>
2022-12-17 18:06:46 +0200

    strings - for loop in string (elements)

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ const mystring = "hello world!"
 // console.log(mystring[1])
 
 for (let i in mystring) {
-    console.log(i) // 0 1 2 3 4 5 6 7 8 9 10 11 (indices)
+    console.log(mystring[i]) // h e ll o w o r l d !(elements)
 }

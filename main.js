d5510bee6d0c6a23e724c06ba9964bbe7978fd60
aris_giotis <apjoti@gmail.com>
2022-12-17 21:59:14 +0200

    delete, ternaries, break, continue - continue

--- main.js
+++ main.js
@@ -4,7 +4,21 @@
 
 for (let i = 0; i < 10; i++) {
     if (i == 5) {
-        break // you get out of the loop
+        continue // omit current iteration, continue with the rest iterations
     }
     console.log(i)
 }
+
+// continue: commonly used with while loops
+let i = 0
+
+while (true) {
+    i++;
+    if (i == 3) continue;
+    if (i > 5) break;
+    console.log(i); // 1 2 (3 get's continued) 4 5 (broke out)
+}
+
+// if i++ was at the bottom -> infinite loop
+
+// JS is asynchronous: like having threads to do jobs in parallel

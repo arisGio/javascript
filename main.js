8e60d1664a771b1a6af4d6defe9cdc9e8cc57e00
aris_giotis <apjoti@gmail.com>
2022-12-17 19:29:13 +0200

    while, do while, logical operators - do while loop

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 "use strict";
 
-let x = 0;
+let x = 51;
 
-while ( x < 5 ) {
-    console.log(x);
+while ( x < 50 ) { // never entering
+    console.log(x); // nothing
     x++;
 }
+
+do { // runs at least once
+    console.log(x); // 51
+    x++;
+} while (x < 50)

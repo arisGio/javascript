d0186396cf6a21f7be97d7787c5bf0311a018a60
aris_giotis <apjoti@gmail.com>
2022-12-17 22:04:44 +0200

    functions - function definition, function invokation

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 "use strict";
 
-{
-    // block, has a different SCOPE (variable access)
-    console.log('test')
+function hello() {
+    console.log('Hello world!')
 }
+
+// if there was no function definition -> block get's executed by default
+
+// if there is a function definition before the block -> the block does not get executed by default, but we have to invoke it instead
+
+hello()

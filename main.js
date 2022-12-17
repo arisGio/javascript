80f34b0c2473b02e59977b70f92c960e3827a6b6
aris_giotis <apjoti@gmail.com>
2022-12-17 19:21:20 +0200

    while, do while, logical operators - logical operators && (AND), || (OR), ! (NOT)

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 let x = 5, y = 10;
 
-console.log(x == 5) // true
-console.log(x == 15) // false
+console.log(x === 5 && y === 10) // true
+console.log(x === 5 && y === 11) // false
+// BOTH conditions must be true to result in true
 
-console.log(x == '5') // true, type conversion under the hood
-// '5' --> 5 --> 5 == 5 is true
-console.log(x === '5') // false, no type conversion, strict comparison
+console.log(x === 5 || y === 10) // true
+console.log(x === 6 || y === 11) // false
+// AT LEAST on condition must be true
+
+let b = true;
+console.log(b)// true
+console.log(!b)// false
+console.log(!(x === 5))// false
+console.log(!(x === 15))// true

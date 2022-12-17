e10dab5b1e2e3ce67859bb77ef001d72f4105039
aris_giotis <apjoti@gmail.com>
2022-12-17 19:41:51 +0200

    if & switch - else-if

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 let age = 20;
 
-if (age < 18) {
+if (age < 6) {
+    console.log("You are infant")
+} else if (age < 18) {
     console.log("You are juvenile")
 } else {
-    console.log("You are an adult")
+    console.log("You are adult")
 }

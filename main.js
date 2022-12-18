76644499c4299b688f5b065ad10820b4b2de3d53
aris_giotis <apjoti@gmail.com>
2022-12-19 01:08:49 +0200

    array methods 1/3 - slice(), does not mutate original array

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const a = [1, 2, 3, 1, 'test', 6]
 
-console.log(a.splice(3,2)) // [1, 'test']
+console.log(a.slice(3,5)) // [1, 'test']
+// last element is non-inclusive
 
-console.log(a)  // [1, 2, 3, 6]
+console.log(a)  // [1, 2, 3, 1, 'test', 6]

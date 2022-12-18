af27a7f40a7a69e81978941be5ca387fea7cc865
aris_giotis <apjoti@gmail.com>
2022-12-19 01:01:58 +0200

    array methods 1/3 - shift(), deletes the 1st element

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 
 const a = [1, 2, 3, 1, 'test', 6]
 
-a.pop() // console.log(a.pop()) --> 6
+a.shift() // console.log(a.shift()) --> 1
 
-console.log(a) // [1, 2, 3, 1, 'test']
+console.log(a) // [2, 3, 1, 'test', 6]

6c44b3bf33ed52da686926b9a44e237c74526052
aris_giotis <apjoti@gmail.com>
2022-12-19 00:28:45 +0200

    context, this - window object

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+console.log(this) // Window, root object of browser
+
 function print_text(){
     console.log(this.innerText)
 }
@@ -9,3 +11,5 @@ document.querySelector('#button-one').addEventListener('click', print_text)
 document.querySelector('#button-two').addEventListener('click', print_text)
 
 // works same as before with the anonymous callback functions
+
+// 'this' changes without us realizing it, so we get bugs

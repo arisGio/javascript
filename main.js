d409cdaab646623df5cdbc8fa4eb1a570a40b5fe
aris_giotis <apjoti@gmail.com>
2022-12-19 00:56:40 +0200

    array methods 1/3 - forEach()

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 
 const a = [1, 2, 3, 1, 'test', 6]
 
-for (let i in a){
-    console.log(i) // 0 1 2 3 4 5
-}
-
-for (let i of a){
-    console.log(i) // 1 2 3 1 'test' 6
-}
+a.forEach(function(element){
+    console.log(element + 1)
+})

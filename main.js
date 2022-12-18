bf87f32a5280f231dfdf6c015ec8d26478d51d10
aris_giotis <apjoti@gmail.com>
2022-12-19 00:39:40 +0200

    context, this - this has changed from button to Window

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 "use strict";
 
-// console.log(this)
+console.log(this)
 
-// function print_text(){
-//     console.log(this)
-// }
+function print_text(){
+    let text = this.innerText
+    this.innerText = 'You clicked me!'
+    console.log(this)           // <button>...
+    setTimeout(function(){
+        console.log(this)       // Window
+        this.innerText = text
+    },3000)
+}
 
-// document.querySelector('#button-one').addEventListener('click', print_text)
+document.querySelector('#button-one').addEventListener('click', print_text)
 
-// document.querySelector('#button-two').addEventListener('click', print_text)
-
-console.log('start')
-setTimeout(function(){
-    console.log('end')
-},3000)
+document.querySelector('#button-two').addEventListener('click', print_text)

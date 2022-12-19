28348bdf2f6c7d4733fb40971f564e051daf05df
aris_giotis <apjoti@gmail.com>
2022-12-19 02:14:50 +0200

    asynchronous JS - setTimeout example (1 not 2), AJAX call simulation

--- main.js
+++ main.js
@@ -2,4 +2,8 @@
 
 var a = (function(){return 1})()
 
-console.log(a) // 1
+setTimeout(function(){a++},1000)
+
+console.log(a) // 1, NOT 2
+
+// JS runs on 1 thread, but can start on a new thread if needed
